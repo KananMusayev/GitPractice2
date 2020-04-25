2aea62e5c79ec219883186e4f5062db6bd0ed565
Kanan Musayev <kanan.musayev@lesroches.ch>
2020-04-25 19:27:15 -0400

    first commit

---
+++ src/Salam/Nedeten.java
@@ -0,0 +1,21 @@
+package Salam;
+
+public class Nedeten {
+    public static void main(String[] args) {
+        System.out.println("afkjflsf");
+        System.out.println("afkjflsf");
+
+        System.out.println("afkjflsf");
+
+        System.out.println("afkjflsf");
+
+        System.out.println("afkjflsf");
+        System.out.println("afkjflsf");
+        System.out.println("afkjflsf");
+
+
+
+
+
+    }
+}
---
+++ src/jfhwfe/rwjfwrlferkf.java
@@ -0,0 +1,19 @@
+package jfhwfe;
+
+public class rwjfwrlferkf {
+    public static void main(String[] args) {
+        System.out.println("fkbfjef");
+        System.out.println("fkbfjef");
+
+        System.out.println("fkbfjef");
+
+        System.out.println("fkbfjef");
+        System.out.println("fkbfjef");
+        System.out.println("fkbfjef");
+
+
+
+
+
+    }
+}
